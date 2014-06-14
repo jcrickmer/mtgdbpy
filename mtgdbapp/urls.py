8eb9531890804621a266d7544802609f0118cab7
Jason Crickmer <jcrickmer@gmail.com>
2014-06-14 18:47:47 -0500

    One step closer to card search results. New search yet, but there is pagination on the first 500 cards that we have in the database.

--- mtgdbapp/urls.py
+++ mtgdbapp/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, url
 from mtgdbapp import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
+                       url(r'^$', views.index, name='search'),
                        # ex: /cards/5/
                        url(r'^(?P<multiverseid>\d+)/$', views.detail, name='detail'),
-                       # ex: /polls/5/results/
-                       url(r'^(?P<multiverseid>\d+)/results/$', views.results, name='results'),
+                       # ex: /polls/results/
+                       url(r'^list/$', views.list, name='list'),
                        # ex: /polls/5/vote/
                        url(r'^(?P<multiverseid>\d+)/vote/$', views.vote, name='vote'),
 )
--- mtgdbapp/views.py
+++ mtgdbapp/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from mtgdbapp.models import Card
 
+
 def index(request):
+    return search(request)
+
+
+def search(request):
     card_list = Card.objects.order_by('multiverseid')[:25]
     context = {
         'card_list': card_list,
         }
     return render(request, 'cards/index.html', context)
+
 
 def detail(request, multiverseid):
     try:
@@ -18,8 +25,23 @@ def detail(request, multiverseid):
         raise Http404
     return render(request, 'cards/detail.html', {'card': card})
 
-def results(request, multiverseid):
-    return HttpResponse("You're looking at the results of card %s." % multiverseid)
+
+def list(request):
+    card_list = Card.objects.order_by('multiverseid')[:500]
+    paginator = Paginator(card_list, 25)
+    page = request.GET.get('page')
+    try:
+        cards = paginator.page(page)
+    except PageNotAnInteger:
+        cards = paginator.page(1)
+    except EmptyPage:
+        cards = paginator.page(paginator.num_pages)
+
+    context = {
+        'cards': cards,
+        }
+    return render(request, 'cards/list.html', context)
+
 
 def vote(request, multiverseid):
     return HttpResponse("You're voting on card %s." % multiverseid)
