9648611e8e5a5f9d97789e281bf175eaf5ef4fd2
Jason Crickmer <jcrickmer@gmail.com>
2014-12-08 22:57:51 -0600

    Not certain about this change. I think Django has it wrong. Had to alter the tables by hand after this to allow for autoincrement ids on battle and battletest. ALTER TABLE mtgdbapp_battletest MODIFY COLUMN id INT auto_increment; ALTER TABLE mtgdbapp_battle MODIFY COLUMN id INT auto_increment;'

---
+++ mtgdbapp/migrations/0010_auto_20141209_0454.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mtgdbapp', '0009_auto_20141208_0452'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='battle',
+            name='id',
+            field=models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='battletest',
+            name='id',
+            field=models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True),
+            preserve_default=True,
+        ),
+    ]
