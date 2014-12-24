34ed41202cf46d30bebf62daa49857a25770f37d
Jason Crickmer <jcrickmer@gmail.com>
2014-12-24 01:18:37 -0600

    Not sure why the admin classes were in my model - they don't belong there. Removed them.

--- mtgdbapp/models.py
+++ mtgdbapp/models.py
@@ -16,62 +16,6 @@ from django.utils import timezone
 from mtgdbapp.view_utils import convertSymbolsToHTML
 from django.utils.safestring import mark_safe
 
-class AuthGroup(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(unique=True, max_length=80)
-    class Meta:
-        managed = True
-        db_table = 'auth_group'
-
-class AuthGroupPermissions(models.Model):
-    id = models.IntegerField(primary_key=True)
-    group_id = models.IntegerField()
-    permission_id = models.IntegerField()
-    class Meta:
-        managed = True
-        db_table = 'auth_group_permissions'
-
-class AuthPermission(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
-    content_type_id = models.IntegerField()
-    codename = models.CharField(max_length=100)
-    class Meta:
-        managed = True
-        db_table = 'auth_permission'
-
-class AuthUser(models.Model):
-    id = models.IntegerField(primary_key=True)
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField()
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=75)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-    class Meta:
-        managed = True
-        db_table = 'auth_user'
-
-class AuthUserGroups(models.Model):
-    id = models.IntegerField(primary_key=True)
-    user_id = models.IntegerField()
-    group_id = models.IntegerField()
-    class Meta:
-        managed = True
-        db_table = 'auth_user_groups'
-
-class AuthUserUserPermissions(models.Model):
-    id = models.IntegerField(primary_key=True)
-    user_id = models.IntegerField()
-    permission_id = models.IntegerField()
-    class Meta:
-        managed = True
-        db_table = 'auth_user_user_permissions'
-
 class Color(models.Model):
     id = models.CharField(primary_key=True, max_length=1)
     color = models.CharField(max_length=9)
@@ -315,33 +259,3 @@ class CardRating(models.Model):
 	def __unicode__(self):
 		return "[CardRating " + str(self.id) + ": " + str(self.physicalcard.id) + " mu=" + str(self.mu) + " sigma=" + str(self.sigma) + " for format \"" + str(self.format.format) + ", test \"" + str(self.test.name) + "\"]"
 	
-class DjangoAdminLog(models.Model):
-	id = models.IntegerField(primary_key=True)
-	action_time = models.DateTimeField()
-	user_id = models.IntegerField()
-	content_type_id = models.IntegerField(blank=True, null=True)
-	object_id = models.TextField(blank=True)
-	object_repr = models.CharField(max_length=200)
-	action_flag = models.IntegerField()
-	change_message = models.TextField()
-	class Meta:
-		managed = True
-		db_table = 'django_admin_log'
-
-class DjangoContentType(models.Model):
-	id = models.IntegerField(primary_key=True)
-	name = models.CharField(max_length=100)
-	app_label = models.CharField(max_length=100)
-	model = models.CharField(max_length=100)
-	class Meta:
-		managed = True
-		db_table = 'django_content_type'
-
-class DjangoSession(models.Model):
-	session_key = models.CharField(primary_key=True, max_length=40)
-	session_data = models.TextField()
-	expire_date = models.DateTimeField()
-	class Meta:
-		managed = True
-		db_table = 'django_session'
-
