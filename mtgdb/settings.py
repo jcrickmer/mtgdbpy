3d1bab5a429134e61988abc788cdafc66cf5ed67
Jason Crickmer <jcrickmer@gmail.com>
2014-12-23 23:54:35 -0600

    Updated wesite app files to use a merged version of the skeletons from Django 1.7. No real changes here, but did it for completeness.

--- mtgdb/settings.py
+++ mtgdb/settings.py
@@ -1,11 +1,11 @@
 """
-Django settings for mtgdbpy project.
+Django settings for mtgdb project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+https://docs.djangoproject.com/en/1.7/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,17 +14,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fn+&ta$dafk$a57ozf9h*!@!j-&np6_ik-%tg#y=$z*@$6b^@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'card.ninja', '172-31-44-78']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mtgdbapp',
+	'mtgdbapp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -44,41 +44,36 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'mtgdbpy.urls'
+ROOT_URLCONF = 'mtgdb.urls'
 
-WSGI_APPLICATION = 'mtgdbpy.wsgi.application'
+WSGI_APPLICATION = 'mtgdb.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mtgdbpy',
-        #'USER': 'mtgdb',
-        #'PASSWORD': 'password'
-        'USER': 'root',
-        'PASSWORD': 'godzilla'
-    },
-    'test': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mtgdbpy_test',
-        'USER': 'mtgdb',
-        'PASSWORD': 'password'
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'mtgdbpy',
+		'USER': 'mtgdb',
+		'PASSWORD': 'password'
+		},
+	'test': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'mtgdbpy_test',
+		'USER': 'mtgdb',
+		'PASSWORD': 'password'
     }
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -91,47 +86,32 @@ USE_L10N = True
 USE_TZ = True
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'handlers': {
         #'file': {
         #    'level': 'DEBUG',
         #    'class': 'logging.FileHandler',
         #    'filename': '/tmp/debug.log',
         #},
-        'applogfile': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join('/tmp/', 'mtgdbpy_debug.log'),
-            'maxBytes': 1024*1024*15, # 15MB
-            'backupCount': 10,
-        },
-        'console':{
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
+		'console':{
+			'level': 'DEBUG',
+			'class': 'logging.StreamHandler',
             #'formatter': 'simple'
         },
 	},
     'loggers': {
-        'django.request': {
-            'handlers': ['console'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
         'mtgdbapp.views': {
             'handlers': ['console'],
             'level': 'DEBUG',
             'propagate': True,
         },
-        'APPNAME': {
-            'handlers': ['applogfile',],
-            'level': 'DEBUG',
-        },
     },
 }
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/opt/mtgdbpy/cstatic/"
+STATIC_ROOT = "/home/jason/projects/mtgdbpy/cstatic/"
+
--- mtgdb/urls.py
+++ mtgdb/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.conf.urls.static import static
-
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^cards/admin/', include(admin.site.urls)),
-    url(r'^cards/', include('mtgdbapp.urls', namespace="cards")),
-    (r'^cn/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/opt/mtgdbpy/cn'}),
-    (r'^img/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/opt/mtgstats/card_images'}),
-)
+					   url(r'^cards/admin/', include(admin.site.urls)),
+					   url(r'^cards/', include('mtgdbapp.urls', namespace="cards")),
+					   (r'^cn/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/jason/projects/mtgdbpy/cn'}),
+					   (r'^img/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/jason/projects/mtgstats/card_images'}),
+    # Examples:
+    # url(r'^$', 'mtgdb.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
+    url(r'^admin/', include(admin.site.urls)),
+)
