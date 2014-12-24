30eff079e30d6fa728154ce7d1a5cfd88805426c
Jason Crickmer <jcrickmer@gmail.com>
2014-12-24 01:54:54 -0600

    Added more base data for migrations.

---
+++ mtgdbapp/migrations/0016_delete_djangosession.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mtgdbapp', '0015_auto_20141224_0719'),
+    ]
+
+    operations = [
+#        migrations.DeleteModel(
+#            name='DjangoSession',
+#        ),
+    ]
