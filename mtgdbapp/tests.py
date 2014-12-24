fd14b9ea5a6ddd468628a61c405597a5d15d80b5
Jason Crickmer <jcrickmer@gmail.com>
2014-12-24 01:12:05 -0600

    LOTS of work to get data migration working so that tests could work. Turns out that Django is doing something really stupid with deleting intial data between test runs. Took 3+ hours to figure this out. See https://code.djangoproject.com/ticket/22487

--- mtgdbapp/tests.py
+++ mtgdbapp/tests.py
@@ -1,8 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+
+from mtgdbapp.models import Color, Rarity
 
 # Create your tests here.
 
-class ModelsTestCase(TestCase):
-	def test_basic_addition(self)
+class ModelsTestCase(TransactionTestCase):
+	serialized_rollback = True
+	#def test_basic_addition(self):
+	#	serialized_rollback = True
+	#	self.assertEqual(1 + 1, 2)
+
+	def test_colors(self):
+		serialized_rollback = True
+		white = Color.objects.get(pk='W')
+		blue = Color.objects.get(pk='U')
+		black = Color.objects.get(pk='B')
+		red = Color.objects.get(pk='R')
+		green = Color.objects.get(pk='G')
+		colorless = Color.objects.get(pk='c')
+		colors = Color.objects.all()
+		self.assertEqual(len(colors), 6)
+
+	def test_rarities(self):
+		serialized_rollback = True
+		b = Rarity.objects.get(pk='b')
+		self.assertEqual(b.sortorder, 0)
+		c = Rarity.objects.get(pk='c')
+		self.assertEqual(c.sortorder, 1)
+		u = Rarity.objects.get(pk='u')
+		self.assertEqual(u.sortorder, 2)
+		r = Rarity.objects.get(pk='r')
+		self.assertEqual(r.sortorder, 3)
+		m = Rarity.objects.get(pk='m')
+		self.assertEqual(m.sortorder, 4)
+		s = Rarity.objects.get(pk='s')
+		self.assertEqual(s.sortorder, 5)
+		rarities = Rarity.objects.all()
+		self.assertEqual(len(rarities), 6)
+
+class ViewsTestCase(TestCase):
+	def test_basic_addition(self):
 		self.assertEqual(1 + 1, 2)
-		
