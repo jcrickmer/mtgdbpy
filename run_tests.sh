dc63597a31da255a15fceeb1dfc688e26b9f3ded
Jason Crickmer <jcrickmer@gmail.com>
2015-04-26 22:50:20 -0500

    Since tests can't be run together because the database does not get refresshed, added a quick little script to run all of the tests.

---
+++ run_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./manage.py test cards.tests.helpertests && ./manage.py test cards.tests.tests && ./manage.py test cards.tests.loadertests && ./manage.py test cards.tests.searchtests && ./manage.py test decks.tests.tests && ./manage.py test decks.tests.parsertests && ./manage.py test decks.tests.stattests
