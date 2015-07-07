2799fdd0d385b80e89fb565ed4771828e2f69e0e
Jason Crickmer <jcrickmer@gmail.com>
2015-07-06 22:29:18 -0500

    Quick hack to have one of the cards in the battle be from a named list of cards. Goal is to make battling more fun because you see cards that have more consequence. This comes at the expense of rigorous sceince, though.

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ mtgdbpy
 You will need to get the right schema into the Solr install. Just using the out-of-the-box example configuration for Solr:
 ./manage.py build_solr_schema > ../solr/solr-4.10.3/example/solr/collection1/conf/schema.xml 
 ./manage.py rebuild_index
+
+
+For Better Battles, create a file with te format id in it called 'betterbattle_{id}.csv'. It should have 1 column of physicalcard.id numbers.
+
+To get it from tournamentdecks in the database:
+
+    echo "SELECT DISTINCT(dc.physicalcard_id) FROM deckcard dc JOIN tournamentdeck td ON td.deck_id = dc.deck_id JOIN tournament t ON t.id = td.tournament_id WHERE t.format_id = 21 AND t.start_date > '2014-12-20';" | mysql -u root -p mtgdbpy > /opt/mtgdb/betterbattle_21.csv
+
+Or, create a file with card names and run this:
+
+    ./manage.py getphysicalcardid < tinyleader_cards.txt | sort | uniq > betterbattle_19.csv
--- cards/views.py
+++ cards/views.py
@@ -15,6 +15,7 @@ from haystack.query import SearchQuerySet
 
 import random
 import operator
+import os.path
 
 from cards.models import Card, CardManager, SearchPredicate, SortDirective, BaseCard
 from cards.models import Mark
@@ -636,7 +637,22 @@ def battle(request, format="redirect"):
             logger.error("Battle: bad ju-ju finding card ratings for basecard id " + str(card_a.basecard.id))
             pass
     else:
-        fcsqls = 'SELECT fbc.basecard_id, cr.mu, cr.sigma, RAND() r FROM formatbasecard fbc JOIN basecard bc ON bc.id = fbc.basecard_id JOIN cardrating cr ON cr.physicalcard_id = bc.physicalcard_id JOIN physicalcard AS pc ON bc.physicalcard_id = pc.id WHERE pc.layout IN %(layouts)s AND fbc.format_id = %(formatid)s ORDER BY r ASC LIMIT 1'
+        bb_file = 'betterbattle_{}.csv'.format(str(format_id))
+        fcsqls_xtra = ''
+        if os.path.isfile(bb_file):
+            bb_ids_list = list()
+            with open(bb_file) as bb_f:
+                bbcontent = bb_f.readlines()
+                for bbline in bbcontent:
+                    try:
+                        bb_ids_list.append(int(bbline.strip()))
+                    except ValueError:
+                        pass
+            query_params['x_ids'] = bb_ids_list
+            if len(bb_ids_list) > 0:
+                fcsqls_xtra = ' AND pc.id IN %(x_ids)s '
+        fcsqls = 'SELECT fbc.basecard_id, cr.mu, cr.sigma, RAND() r FROM formatbasecard fbc JOIN basecard bc ON bc.id = fbc.basecard_id JOIN cardrating cr ON cr.physicalcard_id = bc.physicalcard_id JOIN physicalcard AS pc ON bc.physicalcard_id = pc.id WHERE pc.layout IN %(layouts)s AND fbc.format_id = %(formatid)s {} ORDER BY r ASC LIMIT 1'.format(
+            fcsqls_xtra)
         #logger.error("First Card SQL: " + fcsqls)
         cursor.execute(fcsqls, params=query_params)
         rows = cursor.fetchall()
