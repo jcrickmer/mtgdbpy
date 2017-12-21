84d43b748970c690f5b924f8d1ab074e3689197b
Jason Crickmer <jcrickmer@gmail.com>
2017-12-21 07:47:19 -0600

    Fixed arguements to bring it up to Django 1.11.

--- decks/management/commands/calcstats.py
+++ decks/management/commands/calcstats.py
@@ -19,43 +19,44 @@ from dateutil.parser import parse as dtparse
 class Command(BaseCommand):
     help = '''Recalculate all of the card stats.'''
 
-    option_list = BaseCommand.option_list + (make_option('-a', '--all',
-                                                         dest='all_stats',
-                                                         action='store_true',
-                                                         default=False,
-                                                         help='Calc both formats and cards.'),
-                                             )
-    option_list = option_list + (make_option('-f', '--formats',
-                                             dest='format_stats',
-                                             action='store_true',
-                                             default=False,
-                                             help='Calc stats for formats.'),
-                                 )
-    option_list = option_list + (make_option('-c', '--cards',
-                                             dest='card_stats',
-                                             action='store_true',
-                                             default=False,
-                                             help='Calc stats for cards.'),
-                                 )
-    option_list = option_list + (make_option('-n', '--new-only',
-                                             dest='new_only',
-                                             action='store_true',
-                                             help='Only calculate formats or cards that have not been calculated yet.'),
-                                 )
-    option_list = option_list + (make_option('-s', '--start-date',
-                                             dest='start_date',
-                                             default='2014-09-01',
-                                             help='Start with formats that are on or after this date.'),
-                                 )
-    option_list = option_list + (make_option('-e', '--end-date',
-                                             dest='end_date',
-                                             default='2025-12-31',
-                                             help='End with formats that are on or before this date.'),
-                                 )
-    option_list = option_list + (make_option('--formatname',
-                                             dest='formatname',
-                                             help='Calculate stats for specific format.'),
-                                 )
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-a', '--all',
+            dest='all_stats',
+            action='store_true',
+            default=False,
+            help='Calc both formats and cards.')
+
+        parser.add_argument('-f', '--formats',
+                            dest='format_stats',
+                            action='store_true',
+                            default=False,
+                            help='Calc stats for formats.')
+
+        parser.add_argument('-c', '--cards',
+                            dest='card_stats',
+                            action='store_true',
+                            default=False,
+                            help='Calc stats for cards.')
+
+        parser.add_argument('-n', '--new-only',
+                            dest='new_only',
+                            action='store_true',
+                            help='Only calculate formats or cards that have not been calculated yet.')
+
+        parser.add_argument('-s', '--start-date',
+                            dest='start_date',
+                            default='2014-09-01',
+                            help='Start with formats that are on or after this date.')
+
+        parser.add_argument('-e', '--end-date',
+                            dest='end_date',
+                            default='2025-12-31',
+                            help='End with formats that are on or before this date.')
+
+        parser.add_argument('--formatname',
+                            dest='formatname',
+                            help='Calculate stats for specific format.')
 
     def handle(self, *args, **options):
         sdate = None
