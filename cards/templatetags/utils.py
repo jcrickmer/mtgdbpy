46813971cb0c3176d34bdda78f1039b0b2463da0
Jason Crickmer <jcrickmer@gmail.com>
2018-01-27 13:27:23 -0600

    After some Google indexing, found a couple of improvements to make to have results appear better in search results. More testing and work still required here.

--- cards/templatetags/utils.py
+++ cards/templatetags/utils.py
@@ -4,3 +4,16 @@ from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def remove_whitespace(text):
+    result = text.replace(u"\r", u" ")
+    result = result.replace(u"\n", u" ")
+    breaker = 0
+    while result.find(u"  ") > -1:
+        if breaker > 100:
+            break
+        result = result.replace(u"  ", u" ")
+        breaker = breaker + 1
+    result = result.strip()
+    return result
