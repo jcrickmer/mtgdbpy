8ebf741fcbd2abb633e8ac8caf058970b9991854
jcrickmer <jcrickmer@gmail.com>
2019-06-12 10:36:34 -0500

    Made the ramdisk part of the run_tests script be optional, and fixed a couple of other minor details. Preparing script to be more parameter-driven so that only certain tests can be run.

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -xEe
 
+USE_RAMDISK=0
+
 rm -rf /tmp/testramdisk
 mkdir /tmp/testramdisk
 
-echo "Creating ramdisk..."
-mount -t tmpfs -o size=512m tmpfs /tmp/testramdisk
+if [ $USER_RAMDISK -ne 0 ]; then
+    echo "Creating ramdisk..."
+    mount -t tmpfs -o size=512m tmpfs /tmp/testramdisk
+fi
 
 echo "[mysqld]" > /tmp/testramdisk/my.cnf
 echo "datadir=/tmp/testramdisk/data" >> /tmp/testramdisk/my.cnf
@@ -18,10 +22,11 @@ echo "character-set-filesystem = utf8" >> /tmp/testramdisk/my.cnf
 echo "[mysqld_safe]" >> /tmp/testramdisk/my.cnf
 echo "log-error=/tmp/testramdisk/mysqld.log" >> /tmp/testramdisk/my.cnf
 echo "pid-file=/tmp/testramdisk/mysqld.pid" >> /tmp/testramdisk/my.cnf
-chown -R mysql.mysql /tmp/testramdisk
+MYSQL_USER_PARAM=
+#chown -R mysql.mysql /tmp/testramdisk
 
 echo "Initializing MySQL..."
-mysql_install_db --user=mysql --datadir="/tmp/testramdisk/data"
+mysql_install_db $MYSQL_USER_PARAM --datadir="/tmp/testramdisk/data"
 
 mysqld_safe --defaults-file=/tmp/testramdisk/my.cnf &
 sleep 3
@@ -30,23 +35,35 @@ mysqladmin -S /tmp/testramdisk/mysql.sock -u root password tester
 echo "Running tests..."
 source /home/jason/venvs/mtgdb2/bin/activate
 
-./manage.py test --settings=mtgdb.settings_test cards.tests.helpertests && \
-    ./manage.py test --settings=mtgdb.settings_test cards.tests.tests && \
-    ./manage.py test --settings=mtgdb.settings_test cards.tests.cardmanagertests && \
-    ./manage.py test --settings=mtgdb.settings_test cards.tests.filingstringtests && \
-    ./manage.py test --settings=mtgdb.settings_test cards.tests.loadertests && \
-    ./manage.py test --settings=mtgdb.settings_test cards.tests.searchtests && \
-    ./manage.py test --settings=mtgdb.settings_test cards.tests.nametests && \
-    ./manage.py test --settings=mtgdb.settings_test decks.tests.tests && \
-    ./manage.py test --settings=mtgdb.settings_test decks.tests.parsertests && \
-    ./manage.py test --settings=mtgdb.settings_test decks.tests.stattests && \
-    ./manage.py test --settings=mtgdb.settings_test cards.tests.formattests
+TESTS=()
+TESTS+=('cards.tests.helpertests')
+TESTS+=('cards.tests.tests')
+TESTS+=('cards.tests.cardmanagertests')
+TESTS+=('cards.tests.filingstringtests')
+TESTS+=('cards.tests.loadertests')
+TESTS+=('cards.tests.searchtests')
+TESTS+=('cards.tests.nametests')
+TESTS+=('decks.tests.tests')
+TESTS+=('decks.tests.parsertests')
+TESTS+=('decks.tests.stattests')
+TESTS+=('cards.tests.formattests')
+
+for test_name in "${TESTS[@]}"
+do
+    ./manage.py test --settings=mtgdb.settings_test $test_name || break
+#    if [ $? -ne 0 ]; then
+#        break
+#    fi
+done
 
 echo "Tests completed!"
 echo "Shutting down MySQL..."
 kill `cat /tmp/testramdisk/mysqld.pid` && sleep 6
-echo "Unmounting ramdisk..."
 
+if [ $USER_RAMDISK -ne 0 ]; then
+    echo "Unmounting ramdisk..."
+    umount /tmp/testramdisk
+fi
 
 rm -rf /tmp/testramdisk
 echo "Completed!"
